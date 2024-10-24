dd70d14a7ad5e5a787b05685767725ca8b8a00b8
wisaulni <186232390+wisaulni@users.noreply.github.com>
2024-10-24 22:00:15 +0000

    attempt to force fix

--- packages/roosterjs-content-model-dom/lib/modelApi/selection/setSelection.ts
+++ packages/roosterjs-content-model-dom/lib/modelApi/selection/setSelection.ts
@@ -41,7 +41,7 @@ function setSelectionToBlockGroup(
 
         group.blocks.forEach((block, i) => {
             isInSelection = handleSelection(isInSelection, block, start, end, isInSelection => {
-                return setSelectionToBlock(block, isInSelection, start, end);
+                return setSelectionToBlock(block, isInSelection, start, end2);
             });
 
             if (block.blockType == 'Paragraph' && block.segments.length == 0 && block.isImplicit) {
